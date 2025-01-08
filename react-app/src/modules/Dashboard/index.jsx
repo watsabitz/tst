f915992f19221e7c593c768fc7ca57409d248a22
watsabitz <watsabitz@gmail.com>
2025-01-08 18:28:20 +0200

    zzz

--- react-app/src/modules/Dashboard/index.jsx
+++ react-app/src/modules/Dashboard/index.jsx
@@ -1,38 +1,73 @@
 import { useEffect, useState } from "react";
 import { AgCharts } from "ag-charts-react";
 import Select from "react-select";
+import classes from "./index.module.scss";
+import classnames from "classnames";
 
-const ChartExample = ({ fetchedData = {}, selectedOptions }) => {
+const BarChart = ({
+  fetchedData = {},
+  selectedOptions,
+  currentPage,
+  pageSize,
+}) => {
   const [chartOptions, setChartOptions] = useState({});
 
   useEffect(() => {
     const config = {
+      title: {
+        text: "Frequency of the most common character",
+      },
       data: [],
-      series: [],
+      series: [
+        {
+          type: "bar",
+          xKey: "character",
+          yKey: `sum`,
+        },
+      ],
+      axes: [
+        {
+          type: "category",
+          position: "bottom",
+          title: {
+            text: "Character",
+          },
+        },
+        {
+          type: "number",
+          position: "left",
+          title: {
+            text: "Amount",
+          },
+        },
+      ],
     };
-    for (const [key, value] of Object.entries(fetchedData)) {
-      config.series.push({
-        type: "bar",
-        xKey: `sample`,
-        yKey: `${key}Frequency`,
-        yName: key,
-      });
-      value.slice(0, 20).forEach((value, i) => {
-        if (!config.data[i]) {
-          config.data[i] = { sample: i, [`${key}Frequency`]: value };
-        } else {
-          config.data[i][`${key}Frequency`] = value;
-        }
-      });
+
+    const startIndex = (currentPage - 1) * pageSize;
+
+    if (Object.keys(fetchedData) === 0) {
+      config.data = [];
+    } else {
+      for (const [key, value] of Object.entries(fetchedData)) {
+        const slicedValues = value.splice(startIndex, pageSize);
+        const sum = slicedValues.reduce(
+          (prevValue, currentValue) => prevValue + currentValue,
+          0
+        );
+        config.data.push({ character: key, sum });
+      }
     }
+
     setChartOptions(config);
-  }, [fetchedData]);
+  }, [fetchedData, currentPage, pageSize]);
 
   return <AgCharts options={chartOptions} />;
 };
 
 const Dashboard = () => {
   const pageSize = 200;
+  const currentPage = 2;
+
   const [options, setOptions] = useState([]);
   const [selectedOptions, setSelectedOptions] = useState([]);
   const [fetchedData, setFetchedData] = useState([]);
@@ -74,25 +109,30 @@ const Dashboard = () => {
   }, [selectedOptions, setFetchedData]);
 
   return (
-    <div className="w-full p-4">
-      <div className="w-full flex my-10 ">
-        <label htmlFor="fields">Select fields</label>
-        <Select
-          id="fields"
-          isMulti
-          name="fields"
-          options={options}
-          className="w-1/2"
-          value={selectedOptions}
-          onChange={handleChange}
-        />
+    <div className={classnames(classes.main, "w-full p-4")}>
+      <div className={classnames(classes.leftSide)}>
+        <div className={classnames(classes.header, "w-full flex my-10 gap-4")}>
+          <label htmlFor="fields">Select fields</label>
+          <Select
+            id="fields"
+            isMulti
+            name="fields"
+            options={options}
+            className="w-1/2"
+            value={selectedOptions}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="w-1/2">
+          <BarChart
+            fetchedData={fetchedData}
+            selectedOptions={selectedOptions}
+            currentPage={currentPage}
+            pageSize={pageSize}
+          />
+        </div>
       </div>
-      <div>
-        <ChartExample
-          fetchedData={fetchedData}
-          selectedOptions={selectedOptions}
-        />
-      </div>
+      <div className={classnames(classes.leftSide)}>right side</div>
     </div>
   );
 };
